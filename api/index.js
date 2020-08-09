1b194534ed0d55f178a79474cb9f356c07c93ab2
Pedro Sierra <pedroluis1280@gmail.com>
2020-08-09 14:52:15 -0500

    Add dotenv package and .env.example file

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const { PORT: port = 3000 } = process.env;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
